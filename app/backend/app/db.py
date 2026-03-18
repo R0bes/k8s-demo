32bd8e38228dd4b19b8f4ea9645d01c06e05488a
R0bes <rob.schwe@gmail.com>
2026-03-18 06:31:51 +0100

    add sqlite db for be ci tests

--- app/backend/app/db.py
+++ app/backend/app/db.py
@@ -3,7 +3,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-# postgresql+psycopg://demo-user:demo-password@127.0.0.1:5432/appdb
 DATABASE_URL = os.getenv("DB_URL", "")
 
 print(f">> DB_URL: {DATABASE_URL}")
